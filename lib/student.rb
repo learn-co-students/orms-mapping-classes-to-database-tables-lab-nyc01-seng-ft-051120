aed2dfe711394b6a6d54850199016ffb99db8407
cathjime <xcatherine.jimenez@gmail.com>
2020-05-23 13:36:29 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,46 +1,96 @@
 class Student
-
   attr_accessor :name, :grade
   attr_reader :id
- 
-  def initialize(name, grade, id=nil)
-    @id = id
+
+  def initialize (name, grade, id = nil)
     @name = name
     @grade = grade
-  end
+    @id = id
+  end 
 
   def self.create_table
-    sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        grade INTEGER
-        )
-        SQL
-    DB[:conn].execute(sql) 
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT, 
+      grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
-    sql =  <<-SQL 
+    sql = <<-SQL
     DROP TABLE students
     SQL
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end 
 
   def save
     sql = <<-SQL
-    INSERT INTO students (name, grade) 
-    VALUES (?, ?)
-  SQL
+    INSERT INTO students(name, grade)
+    VALUES(?,?)
+    SQL
 
-  DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid () FROM students") [0][0]
+  end 
 
-  def self.create(name:, grade:)
+  def self.create (name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
-  end
+  end 
  
 end
+
+
+
+
+
+
+
+
+
+#  attr_accessor :name, :grade
+#   attr_reader :id
+ 
+#   def initialize(name, grade, id=nil)
+#     @id = id
+#     @name = name
+#     @grade = grade
+#   end
+
+#   def self.create_table
+#     sql =  <<-SQL 
+#       CREATE TABLE IF NOT EXISTS students (
+#         id INTEGER PRIMARY KEY, 
+#         name TEXT, 
+#         grade INTEGER
+#         )
+#         SQL
+  #   DB[:conn].execute(sql) 
+  # end
+
+  # def self.drop_table
+  #   sql =  <<-SQL 
+  #   DROP TABLE students
+  #   SQL
+  #   DB[:conn].execute(sql) 
+  # end 
+
+  # def save
+  #   sql = <<-SQL
+  #   INSERT INTO students (name, grade) 
+  #   VALUES (?, ?)
+  # SQL
+
+  # DB[:conn].execute(sql, self.name, self.grade)
+  # @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  # end
+
+  # def self.create(name:, grade:)
+  #   student = Student.new(name, grade)
+  #   student.save
+  #   student
+  # end
